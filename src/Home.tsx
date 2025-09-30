1dc409bacf3b6e4935818dc2579524bef9cc0d69
sidneimoraes <sidneimoraes@prof.educacao.sp.gov.br>
2025-09-29 22:11:45 -0300

    Criacao da navbar e das secoes do portifolio

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,23 @@
 export default function Home() {
   return (
-    <h1 className="fw-bold">Olá, Mundo!</h1>
+    <>
+      <h1 className="fw-bold">Olá, Mundo!</h1>
+
+      <section id="hero">
+        <h2>Início</h2>
+      </section>
+
+      <section id="sobre">
+        <h2>Sobre Mim</h2>
+      </section>
+
+      <section id="projetos">
+        <h2>Projetos</h2>
+      </section>
+
+      <section id="contato">
+        <h2>Contato</h2>
+      </section>
+    </>
   )
 }
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,9 +1,63 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link, Outlet } from "react-router";
 import { Toaster } from "react-hot-toast";
-import { Outlet } from "react-router";
+import { useEffect } from "react";
+
+const navigation = [
+  {
+    key: "sobre",
+    label: "Sobre",
+    link: "#sobre"
+  },
+  {
+    key: "projetos",
+    label: "Projetos",
+    link: "#projetos"
+  },
+  {
+    key: "contato",
+    label: "Contato",
+    link: "#contato"
+  }
+]
 
 export default function Layout() {
+  useEffect(() => {
+    const navbar = document.querySelector(".navbar") as HTMLElement
+    if (navbar) {
+      document.body.style.paddingTop = `${navbar.offsetHeight}px`
+    }
+  }, [])
+
   return (
     <div className="d-flex flex-column min-vh-100">
+      <Navbar expand="sm" fixed="top" className="bg-body-tertiary border-bottom shadow-sm py-3">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <img
+              alt="Logo Vite"
+              src="vite.svg"
+              width={30}
+              height={30}
+              className="d-inline-block align-top"
+            />{" "}
+            Levi Moraes
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav defaultActiveKey="hero" className="me-auto">
+              <Nav.Item>
+                <Nav.Link eventKey="hero" href="#hero">Início</Nav.Link>
+              </Nav.Item>
+              {navigation.map((item) => (
+                <Nav.Item key={item.key}>
+                  <Nav.Link eventKey={item.key} href={item.link}>{item.label}</Nav.Link>
+                </Nav.Item>
+              ))}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <div className="flex-grow-1 p-4">
         <div className="mx-auto" style={{ maxWidth: "56rem" }}>
           <Outlet />
