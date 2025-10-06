76e6fed5fa2a9be5f26b397f3c8c6c9b9c5153be
sidneimoraes <sidneimoraes@prof.educacao.sp.gov.br>
2025-10-06 20:49:15 -0300

    Criacao do styled component Description

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,6 +10,11 @@ const Container = styled.div`
   padding: 0 1.5rem;
 `
 
+const Description = styled.p`
+  margin-bottom: 0;
+  text-align: justify;
+`
+
 export default function Hero() {
   return (
     <section id="inicio" className="wrapper">
@@ -19,9 +24,9 @@ export default function Hero() {
             <h1 className="display-3 text-center text-md-start">Levi Moraes</h1>
           </Col>
           <Col xs="12" md="6">
-            <p className="mb-0" style={{ textAlign: "justify" }}>
+            <Description>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, possimus! Corrupti saepe ex beatae voluptates animi accusantium quaerat dolorum, earum fugiat officiis corporis laborum ut. Unde laborum corporis deserunt voluptas! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, magni. Eius, accusantium? Praesentium reprehenderit facilis doloribus? Labore aperiam fugiat voluptates cupiditate temporibus, consectetur ducimus necessitatibus eveniet adipisci sapiente sed nihil.
-            </p>
+            </Description>
           </Col>
         </Row>
       </Container>
